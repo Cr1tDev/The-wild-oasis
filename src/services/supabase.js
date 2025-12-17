ed8e2ae230bbfff9dd149054d20987d1a7441162
Ivanarre <arre2397@gmail.com>
2025-12-17 13:47:53 +0800

    implemented image upload

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sgljjkqeopcunmlxmrmf.supabase.co";
+export const supabaseUrl = "https://sgljjkqeopcunmlxmrmf.supabase.co";
 const supabaseKey = "sb_publishable_vOjXV6k4rgN8Q8M-TK4w0A_usps7l8n";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
